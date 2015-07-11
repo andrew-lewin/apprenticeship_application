055747ea021e47b1b803151a260a4d4d25dbf7da
Andrew Lewin <andrew@lewin.org>
2015-07-11 14:49:37 -0600

    redo links

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,10 +1,15 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  test "should get home" do
+  test 'should get home' do
     get :home
     assert_response :success
-    assert_select "title", "Home | Apprenticeship App"
+    assert_select 'title', 'Home | Apprenticeship App'
   end
-
+  
+  test 'should get about' do
+    get :about
+    assert_response :success
+    assert_select 'title', 'About | Apprenticeship App'
+  end
 end
